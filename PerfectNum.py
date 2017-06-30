7358b1b0e7e5b5928f407c8d4de05ac6852229a4
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-30 09:35:19 +0800

    Update PerfectNum.py

--- PerfectNum.py
+++ PerfectNum.py
@@ -1,7 +1,7 @@
 import sys
 
 def is_perfect(n):
-    return sum([i for i in range(1,n) if n%i == 0]) == n
+    return sum ([i for i in range (2, n/2 + 1) if n % i == 0]) + 1 == n
 
 for i in range(1,len(sys.argv)):
     print is_perfect (int(sys.argv[i]))
