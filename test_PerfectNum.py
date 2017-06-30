645cb9840ce0eaaee91c8b93aa9697a8b938f3ba
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-30 09:56:03 +0800

    Update test_PerfectNum.py

--- test_PerfectNum.py
+++ test_PerfectNum.py
@@ -12,5 +12,6 @@ class PerfectNumTest(unittest.TestCase):
     def test_is_perfect3(self):
         self.assertEqual(is_perfect(7), False)
 
+        
 if __name__ == '__main__':
     unittest.main()
