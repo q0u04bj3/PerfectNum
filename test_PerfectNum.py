bb83c42eb284b92d068f7703311db0242fb81441
Pan Yan Ru <q0u04bj3@gmail.com>
2017-06-30 09:35:54 +0800

    Add files via upload

---
+++ test_PerfectNum.py
@@ -0,0 +1,16 @@
+import unittest
+from PerfectNum import is_perfect
+
+class PerfectNumTest(unittest.TestCase):
+    
+    def test_is_perfect1(self):
+        self.assertEqual(is_perfect(5), False)
+    
+    def test_is_perfect2(self):
+        self.assertEqual(is_perfect(6), True)
+
+    def test_is_perfect3(self):
+        self.assertEqual(is_perfect(7), False)
+
+if __name__ == '__main__':
+    unittest.main()
